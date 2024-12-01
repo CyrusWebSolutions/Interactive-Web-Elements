5b3634cc54961d6714b2832d9f073a78e62116d9
CyrusWebSolutions <cyruswebsolutions@gmail.com>
2024-12-01 17:39:40 -0500

    Practiced Carousel Element

---
+++ Content Display and Organization/Carousel Elements/Slideshow_Basic2_Att1/slideshow.js
@@ -0,0 +1,34 @@
+"use strict";
+
+let currentSlide = 0;
+
+function showSlide(index) {
+  if (index >= images.length) {
+    currentSlide = 0;
+
+  } else if (index < 0) {
+    currentSlide = images.length - 1;
+
+  } else {
+    currentSlide = index;
+    
+  }
+
+  const slideImage = document.getElementById("slide-image");
+  const caption = document.getElementById("caption");
+
+  slideImage.style.backgroundImage = `url(${images[currentSlide]})`;
+  caption.textContent = imgCaptions[currentSlide];
+}
+
+function nextSlide() {
+  showSlide(currentSlide + 1);
+}
+function prevSlide() {
+  showSlide(currentSlide - 1);
+}
+
+document.getElementById("next").addEventListener("click", nextSlide);
+document.getElementById("prev").addEventListener("click", prevSlide);
+
+showSlide(currentSlide);
